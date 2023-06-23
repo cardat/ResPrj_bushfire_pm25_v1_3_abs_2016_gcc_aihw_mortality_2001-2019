73c6052e938523f62668e341c3e181694030a167
Lucas Hertzog <291828H@curtin.edu.au>
2023-06-23 16:52:23 +0800

    plot mortality

--- R/do_plot_mortality_gcc.R
+++ R/do_plot_mortality_gcc.R
@@ -14,12 +14,15 @@ do_plot_mortality_gcc <- function(
   titles <- c("Australian Capital Territory", "Adelaide", "Brisbane", "Darwin", "Hobart", "Melbourne", "Perth", "Sydney")
   
   par(mfrow = c(length(gccs), 1),
-      mar = c(0.2, 4, 0.2, 0.2),
+      mar = c(0.2, 2, 0.2, 0.2),
       mgp = c(2.5, 1, 0),
+      oma = c(4, 4, 0, 0),
       las = 1,
       cex.axis = 0.8,
       cex.lab = 1)
   
+  
+  ## iterate through gccs ##
   for (i in 1:length(gccs)) {
     gcc <- gccs[i]
     title_text <- titles[i]
@@ -27,8 +30,7 @@ do_plot_mortality_gcc <- function(
     dat <- get(gcc)
     
     if (i == length(gccs)) {
-      par(mar = c(3.5, 4, 0.2, 0.2))  # Increase bottom margin for x-axis label
-      
+      ## last plot ##   
       plot(avg_doy_all ~ doy, dat, pch = 19, col = grey(0.8), cex = 0.5,
            ylab = "Daily deaths", xlab = "Day of the year", main = "")
       
@@ -36,8 +38,7 @@ do_plot_mortality_gcc <- function(
       
       lines(1:366, tapply(dat$avg_doy_all, dat$doy, mean), lwd = 1.5, col = 4)
     } else {
-      plot(avg_doy_all ~ doy, dat, xaxt = "n", pch = 19, col = grey(0.8), cex = 0.5,
-           ylab = "Daily deaths", main = "")
+      plot(avg_doy_all ~ doy, dat, xaxt = "n", pch = 19, col = grey(0.8), cex = 0.5)
       
       title(title_text, adj = 0.99, line = -1.5)
       
@@ -45,5 +46,16 @@ do_plot_mortality_gcc <- function(
     }
     # Add horizontal and vertical grid lines
     grid(nx = NULL, ny = NULL, lty = "dashed", col = "grey")
+    
+    ## add x legend
+    mtext("Day of the Year", side = 1, line = 2, outer = TRUE)
+    
+    ## add y legend
+    if (i == 4) {
+      ## Calculate the mean point
+      mean_point <- mean(par("usr")[3:4]) 
+      ## Insert text
+      text(par("usr")[1] - 20, mean_point, labels = "Daily deaths", xpd = NA, srt = 90, cex = 1.8)  
+    }
   }
 }
